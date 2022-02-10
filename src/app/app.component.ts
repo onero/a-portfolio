6d41cfcedcaf4999476f7f3a99e37c2f11eabac9
Adam Hansen <onero@users.noreply.github.com>
2022-02-10 17:07:06 +0100

    Added analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'a-portfolio';
+
+  ngOnInit(): void {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    getAnalytics(app);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,16 @@ export const environment = {
   baseUrl: `http://localhost:4200/`
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyBx4ToKRbQ9zysK735e1ZWyULId3QFZUQ8",
+  authDomain: "adamino-portfolio.firebaseapp.com",
+  projectId: "adamino-portfolio",
+  storageBucket: "adamino-portfolio.appspot.com",
+  messagingSenderId: "846476820539",
+  appId: "1:846476820539:web:3bde2d1cd9391e37e59c1e",
+  measurementId: "G-QNE50RTMSD"
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.

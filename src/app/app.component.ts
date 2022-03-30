9cd18c183366625dc1640c0f820496b8fb30f776
Adam Hansen <onero@users.noreply.github.com>
2022-03-30 21:52:53 +0200

    Improved Lighthouse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { Meta } from '@angular/platform-browser';
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from 'src/environments/environment';
 
 @Component({
@@ -10,6 +11,20 @@ import { firebaseConfig } from 'src/environments/environment';
 })
 export class AppComponent implements OnInit {
   title = 'a-portfolio';
+
+  constructor(meta: Meta) {
+    meta.addTags([
+      {
+        name: 'description',
+        // eslint-disable-next-line max-len
+        content: 'Adam Hansen personlige portfolie'
+      },
+      {
+        name: 'keywords',
+        content: 'cv, portfolio, ionic, firebase, angular'
+      }
+    ]);
+  }
 
   ngOnInit(): void {
     // Initialize Firebase
